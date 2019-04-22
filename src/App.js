d60117fba782359ae38095d60a85aec15ced2dd1
姚辰斓 <ellen25@yaochenlandeMacBook-Pro.local>
2019-04-22 16:11:04 +0800

    side project 001

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Headline from './Headline.js';
+import InputComp from './InputComp.js';
+import Draggable, {DraggableCore} from 'react-draggable';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      words: [],
+    }
+  }
+
+
+  addWord = (newWord) => {
+    let newWords = this.state.words.concat(newWord);
+    this.setState({
+      words: newWords  
+    })
+  }
+
+  showWords = () =>{
+    let paragraphs = this.state.words.map((word) => {
+        return <Draggable><div className="box"> <p id="word">{word}</p> </div></Draggable>
+    });
+    return paragraphs
+    
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="leftPart">
+          <div className="headlines">
+            <Headline text={"TITLE"} smallText1={"LOG IN"} 
+            smallText2={"CATAGORIES"} smallText3={"INFO"}></Headline>
+          </div>
+        </div>
+
+        <div className="rightPart">
+          <InputComp submit={this.addWord}></InputComp>
+
+          <h3>Drag and Drop Your Titles!</h3>
+
+        <div className="container">
+          <div className="graphic">
+            <img src="https://s3-us-west-1.amazonaws.com/sfmoma-media-dev/www-media/2018/11/30000121/Paramount-768x516.jpg"></img>
+          </div>
+          <div className="display">
+              {this.showWords()}
+          </div>
+        </div>
+         
+
+        </div>
+
+
       </div>
     );
   }
